4ccfba0200a57886f49a5590150a48c583cbdb7f
Irvine99 <stanislasgerardin@gmail.com>
2025-04-09 11:51:03 +0200

    enlever node module

--- node_modules/@dashevo/wallet-lib/src/utils/duffsToDash.js
+++
@@ -1,9 +0,0 @@
-const { DUFFS_PER_DASH } = require('../CONSTANTS');
-
-function duffsToDash(duffs) {
-  if (duffs === undefined || duffs.constructor.name !== Number.name) {
-    throw new Error('Can only convert a number');
-  }
-  return duffs / DUFFS_PER_DASH;
-}
-module.exports = duffsToDash;
