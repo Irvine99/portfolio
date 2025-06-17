7b8eb98e99144af4f5f69307ea6231a377ce9ae6
Stanislas <stanislasgerardin@gmail.com>
2025-06-17 10:11:51 +0200

    reafcto meta

--- templates/head.php
+++ templates/head.php
@@ -3,6 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--  -->
+    <meta charset="UTF-8">
+
+    <!-- Open Graph (Facebook, LinkedIn, etc.) -->
+    <meta property="og:title" content="Mon Porfolio" />
+    <meta property="og:description" content="Ce portfolio regroupe à la fois des projets réalisés dans le cadre de ma formation et des initiatives personnelles, qui témoignent de mon engagement, de ma curiosité technique et de ma volonté d’évoluer dans le secteur IT. " />
+    <meta property="og:image" content="https://cdn.pixabay.com/photo/2023/02/09/13/51/space-7778868_1280.jpg" />
+    <meta property="og:url" content="https://www.stanislasgerardin.fr" />
+    <meta property="og:type" content="portfolio" />
+
+    <!-- Twitter Card -->
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="Mon Projet PHP" />
+    <meta name="twitter:description" content="Une petite application PHP développée sous Ubuntu." />
+    <meta name="twitter:image" content="https://exemple.com/images/mon-projet.jpg" />
     <link rel="stylesheet" href="../src/output.css">
     <link rel="stylesheet" href="../src/main.css">
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
@@ -23,5 +39,5 @@
   <!-- IBM script -->
   <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;600&display=swap" rel="stylesheet">
 
-    <title>Document</title>
+    <title>Portfolio Stanislas Gerardin</title>
 </head>
